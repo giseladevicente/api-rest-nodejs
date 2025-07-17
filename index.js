ba0d018526e6bcab4aae68828f1059ad78b8bd32
giseladevicente <devicentegisela@gmail.com>
2025-07-17 14:25:05 -0300

    install and configure CORS

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import "dotenv/config";
 import express from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(cors()); 
 
 app.get('/', (req, res) => {
     res.json({ message: "Bienvenido a API REST Firestore - Proyecto Final Talento Tech"})
